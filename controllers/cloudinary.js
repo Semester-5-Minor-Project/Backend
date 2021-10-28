26f1a22a139040dcf2be5960787fa32064bd6cc2
solarconstant <ashishshubh001@gmail.com>
2021-10-28 23:20:38 +0530

    Add Student Photo Upload and store to Cloudinary

---
+++ controllers/cloudinary.js
@@ -0,0 +1,33 @@
+const cloudinary = require("cloudinary").v2;
+const DatauriParser = require("datauri/parser");
+const parser = new DatauriParser();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+exports.upload = async (req, res, next) => {
+    if (req.files.file.mimetype === "image/png" || req.files.file.mimetype === "image/jpeg") {
+        try {
+            let result = await cloudinary.uploader.upload(req.files.file.tempFilePath, {
+                use_filename: true,
+                resource_type: 'auto'
+            });
+            console.log(result);
+            req.body.public_id = result.public_id;
+            req.body.link = result.secure_url;
+            next();
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+    else {
+        res.status(500).json(
+            {
+                message: "Invalid file type."
+            })
+    }
+}
